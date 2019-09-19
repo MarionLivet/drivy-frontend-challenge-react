e7580bd8f5114accabf22b19c65d7c933f79ea5c
Marion Livet <mlivet93@gmail.com>
2019-09-19 10:36:44 +0200

    level 2: filtering by duration and distance

--- src/components/CarsList.jsx
+++ src/components/CarsList.jsx
@@ -18,23 +18,83 @@ const CarsWrapper = styled.div`
   }
 `;
 
+const Form = styled.div`
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 12px;
+  background: #f7f9f9;
+  border-radius: 5px;
+  padding: 10px 20px;
+  box-sizing: border-box;
+`;
+
+const Input = styled.input`
+  background: #f7f9f9;
+  flex: 1;
+  border-radius: 5px;
+  padding: 5px 8px;
+  border: 1px solid #e2e2e2;
+  font-size: 18px;
+  transition: border 300ms ease;
+
+  &:hover {
+    border: 1px solid #c3c3c3;
+  }
+`;
+
+const Label = styled.label`
+  display: flex;
+  margin: 5px 0;
+  font-size: 18px;
+
+  span {
+    min-width: 20%;
+  }
+`;
+
 const CarsList = () => {
   const [data, setData] = useState([]);
   const [cars, setCars] = useState([]);
+  const [distance, setDistance] = useState(300);
+  const [duration, setDuration] = useState(3);
 
   useEffect(() => {
-    const url = `http://localhost:3001/cars.json`;
-
+    const url = `http://localhost:3001/cars.json?duration=${duration}&distance=${distance}`;
+    
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setData(data);
         setCars(data);
       });
-  }, []);
+  }, [distance, duration]);
 
   return (
     <div>
+      <Form>
+        <Label htmlFor="distance">
+          <span>Distance (in km)</span>
+          <Input
+            id="distance"
+            type="number"
+            value={distance}
+            onChange={(event) => setDistance(event.target.value)}
+            max="3000"
+            min="0"
+          />
+        </Label>
+        <Label htmlFor="duration">
+          <span>Duration (in days)</span>
+          <Input
+            id="duration"
+            type="number"
+            value={duration}
+            onChange={(event) => setDuration(event.target.value)}
+            max="30"
+            min="1"
+          />
+        </Label>
+      </Form>
       <CarsWrapper>
         {cars && cars.map((car, i) => (
           <Car key={car.id} style={{marginBottom: 30}} {...car} />
